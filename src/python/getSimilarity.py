113dc911ad1aaac25ca389930a4abaccf6cb99c7
vishaNagpal <vishakha2507nagpal@gmail.com>
2021-09-01 02:10:13 +0530

    removed unwanted code from main file

--- src/python/getSimilarity.py
+++ src/python/getSimilarity.py
@@ -1,38 +1,8 @@
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-import math
 
 category_list = ['database', 'backendServices', 'automation', 'frontend', 'management'];
-
-
-def get_similarity_with_model(skills):
-    import gensim.downloader as api
-    model = api.load('word2vec-google-news-300')
-    print('reading categories json file')
-    file = open(
-        '/Users/vishakha.nagpal/development/my_workspace/QMapper/src/python/parser/keywords/skillCategories.json', )
-    skillCategories = json.load(file)
-    similarityList = []
-    print(skills)
-    # max_matching_cat = ''
-    # maxSimilarity = -1
-    for cat in skillCategories:
-        distance = model.wmdistance(skills, cat)
-        distance = round(distance, 2)
-        if math.isinf(distance):  # todo: train this model for all technologies as well
-            distance = 0  # todo: fix it up
-        print(distance)
-        # if maxSimilarity >= distance:
-        #     maxSimilarity = distance
-        # max_matching_cat = cat
-        similarityList.append(300 - (distance * 100))
-    # print(similarityList)
-    # plotChart(similarityList,categoryList)
-    # fetchQuestions(skillCategories[0]['backendServices'],6);
-    file.close();
-    return {'similarityList': similarityList, 'categoryList': categoryList}
-
 
 def get_similarity(skills):
     print('reading categories json file')
@@ -47,10 +17,8 @@ def get_similarity(skills):
             break
         count = count+1
         category_key = next(iter(cat))
-        same_items_list = [value.lower() for value in skills if value.lower() in cat[category_key]]
+        same_items_list = [value.lower().strip() for value in skills if value.lower().strip() in cat[category_key]]
         distance = len(same_items_list) * 100
-        if distance == 0:
-            distance = 20
         print(distance)
         similarity_list.append(distance)
         similarity_object[category_key] = {
